2f013a735c25670c763a419bdac55c0b01332200
Luyuan Xing <lxing.nyc@gmail.com>
2016-07-01 16:00:50 -0400

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,6 +3,10 @@ class VisitorsController < ApplicationController
 	def new
 		@owner = Owner.new
 		# render 'visitors/new'
+		# render 'visitors/new', :layout => false
+		# render 'visitors/new', :layout => 'special'
+		flash.now[:notice] = 'Welcome!'
+		flash.now[:alert] = 'My birthday is soon.'
 	end
 
 end
